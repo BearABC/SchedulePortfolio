b910097dfd0d33e19ab622f6c5e309e767436de7
AndroidPM <AndroidPM@192.168.1.12>
2017-04-18 13:47:06 +0900

    t

--- src/main/java/scheduleshare/config/BizConfig.java
+++ src/main/java/scheduleshare/config/BizConfig.java
@@ -1,13 +1,10 @@
 package scheduleshare.config;
 
-import javax.validation.Validator;
-
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
 @ComponentScan("scheduleshare.domain.model.service")
@@ -20,14 +17,5 @@ public class BizConfig {
 		messageSource.setBasename("classpath:/META-INF/messages");
 
 		return messageSource;
-	}
-
-	@Bean
-	public Validator globalValidator(MessageSource messageSource) {
-		LocalValidatorFactoryBean validatorBean
-			= new LocalValidatorFactoryBean();
-		validatorBean.setValidationMessageSource(messageSource);
-
-		return validatorBean;
 	}
 }
--- src/main/java/scheduleshare/config/WebMvcConfig.java
+++ src/main/java/scheduleshare/config/WebMvcConfig.java
@@ -1,9 +1,15 @@
 package scheduleshare.config;
 
+import javax.validation.Validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
@@ -11,7 +17,10 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan("scheduleshare.app.controller")
-public class WebMvcConfig {
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@Bean
 	public ServletContextTemplateResolver templateResolver() {
@@ -37,5 +46,13 @@ public class WebMvcConfig {
 		resolver.setCharacterEncoding("UTF-8");
 		resolver.setOrder(1);
 		return resolver;
+	}
+
+	public Validator globalValidator(MessageSource messageSource) {
+		LocalValidatorFactoryBean validatorBean
+			= new LocalValidatorFactoryBean();
+		validatorBean.setValidationMessageSource(messageSource);
+
+		return validatorBean;
 	}
 }
